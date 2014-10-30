ea5e7495c91361e2055d0dfe8efd6e4d20b1581e
Jordan <Jordan@10.0.130.188>
2014-10-30 10:50:56 -0400

    Added States package, rearranged logic to fit in proper classes.

---
+++ SoftwareEngineeringGameGit/src/Controller/LevelSelect.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+import Model.Level;
+import Model.Tower;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Jordan
+ */
+public class LevelSelect {
+    
+    private void displayunlockTower(){
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+    private void unlockTower(){
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+    private void launchLevel(Level level){
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+    private void save(){
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+    int gold;
+    ArrayList<Tower> availableTowers;
+    
+}
---
+++ SoftwareEngineeringGameGit/src/States/BaseState.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package States;
+
+/**
+ *
+ * @author Jordan
+ */
+public abstract class BaseState {
+
+    private void animationLoop() {
+
+    }
+    
+    private void displayExitMenu(){
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+    private void exitGame(){
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
