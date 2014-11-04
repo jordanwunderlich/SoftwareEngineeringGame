45978cbbc17013e81b3dd0c96912f6b0aa5f24f8
student <student@172.31.35.33>
2014-11-04 11:07:32 -0500

    created controller classes

---
+++ SoftwareEngineeringGameGit/src/Controller/EndOfLevel.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Controller;
+
+import java.awt.event.ActionEvent;
+
+/**
+ *
+ * @author student
+ */
+public class EndOfLevel {
+    
+    public void actionPerformed(ActionEvent click){
+        if(click.getActionCommand().equals("restart round")){
+            clickedRestartRound();
+        }
+    }
+    
+    private void clickedRestartRound () {
+        
+    }
+    private void clickedLevelSelect () {
+        
+    }
+    private void clickedQuit () {
+        
+    }
+    
+}
---
+++ SoftwareEngineeringGameGit/src/Controller/UnlockTower.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Controller;
+
+import java.awt.event.ActionEvent;
+
+/**
+ *
+ * @author student
+ */
+public class UnlockTower {
+    
+    public void actionPerformed(ActionEvent click){
+        if(click.getActionCommand().equals("basic tower")){
+            clickedBasicTower();
+        }
+    }
+    
+    private void clickedBasicTower () {
+        
+    }
+            
+    private void clickedSlowTower () {
+        
+    }
+            
+    private void clickedAreaOfEffectTower () {
+        
+    }
+    private void clickedLongRangeTower () {
+        
+    }
+}
